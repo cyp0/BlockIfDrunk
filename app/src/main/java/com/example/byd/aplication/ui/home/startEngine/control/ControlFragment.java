095018c0dc94444cc375ceef9e16495d8063ce43
Cyp0 <edgar-sanchez11@hotmail.com>
2020-10-15 02:15:29 -0500

    Added bluetooth connection to block if drunk prototype

--- app/src/main/java/com/example/byd/aplication/ui/home/startEngine/control/ControlFragment.java
+++ app/src/main/java/com/example/byd/aplication/ui/home/startEngine/control/ControlFragment.java
@@ -1,29 +1,249 @@
 package com.example.byd.aplication.ui.home.startEngine.control;
 
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
+import com.beardedhen.androidbootstrap.BootstrapButton;
 import com.example.byd.R;
+import com.example.byd.aplication.ui.home.startEngine.StartCartFragment;
+import com.example.byd.databinding.FragmentControlBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+
+
 public class ControlFragment extends Fragment {
 
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
+
+    private ImageButton buttonForward, buttonBackward, buttonTurnLeft, buttonTurnRight,buttonStop, buttonDisconnect;
+
+
+    private BootstrapButton disconnectButton;
+    Handler bluetoothIn;
+    final int handlerState = 0;
+    private BluetoothAdapter btAdapter = null;
+    private BluetoothSocket btSocket = null;
+    private StringBuilder DataStringIN = new StringBuilder();
+    private ConnectedThread MyConexionBT;
+    // Identificador unico de servicio - SPP UUID
+    private static final UUID BTMODULEUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    // String para la direccion MAC
+    private String address = null;
+    //-------------------------------------------
     public ControlFragment() {
-        // Required empty public constructor
+
+    }
+
+    public ControlFragment(String address) {
+        this.address = address;
     }
 
 
+    @SuppressLint("HandlerLeak")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_control, container, false);
+        FragmentControlBinding binding = FragmentControlBinding.inflate(inflater, container, false);
+        fragmentManager = getParentFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+
+        bluetoothIn = new Handler() {
+            public void handleMessage(android.os.Message msg) {
+                if (msg.what == handlerState) {
+
+                    //Interacción con los datos de ingreso
+                }
+            }
+        };
+
+        btAdapter = BluetoothAdapter.getDefaultAdapter(); // get Bluetooth adapter
+        VerificarEstadoBT();
+
+        buttonForward = binding.imageButtonForward;
+        buttonForward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyConexionBT.write("1");
+            }
+        });
+
+        buttonBackward = binding.imageButtonBackward;
+        buttonBackward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyConexionBT.write("2");
+            }
+        });
+
+        buttonTurnLeft = binding.imageButtonTurnLeft;
+        buttonTurnLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyConexionBT.write("4");
+            }
+        });
+
+        buttonTurnRight = binding.imageButtonTurnRight;
+        buttonTurnRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyConexionBT.write("3");
+            }
+        });
+
+        buttonStop = binding.imageButtonStop;
+        buttonStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyConexionBT.write("5");
+            }
+        });
+
+        disconnectButton = binding.buttonDisconnect;
+        disconnectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (btSocket!=null)
+                {
+                    try {btSocket.close();}
+                    catch (IOException e)
+                    { Toast.makeText(getActivity(), "Error", Toast.LENGTH_SHORT).show();;}
+                }
+                fragmentTransaction.replace(R.id.containerOfFragments, new StartCartFragment());
+                fragmentTransaction.commit();
+            }
+        });
+        return binding.getRoot();
+    }
+
+    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException
+    {
+        //crea un conexion de salida segura para el dispositivo usando el servicio UUID
+        return device.createRfcommSocketToServiceRecord(BTMODULEUUID);
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+//ESTO PUEDE DAR ERROR, CAMBIAR A MI VERSION
+//        Intent intent = getIntent();
+//        address = intent.getStringExtra(DispositivosVinculados.EXTRA_DEVICE_ADDRESS);
+        //Setea la direccion MAC
+        BluetoothDevice device = btAdapter.getRemoteDevice(address);
+
+        try
+        {
+            btSocket = createBluetoothSocket(device);
+        } catch (IOException e) {
+            Toast.makeText(getActivity(), "La creacción del Socket fallo", Toast.LENGTH_LONG).show();
+        }
+        // Establece la conexión con el socket Bluetooth.
+        try
+        {
+            btSocket.connect();
+        } catch (IOException e) {
+            try {
+                btSocket.close();
+            } catch (IOException e2) {}
+        }
+        MyConexionBT = new ConnectedThread(btSocket);
+        MyConexionBT.start();
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        try
+        { // Cuando se sale de la aplicación esta parte permite que no se deje abierto el socket
+            btSocket.close();
+        } catch (IOException e2) {}
+    }
+
+    //Comprueba que el dispositivo Bluetooth
+    //está disponible y solicita que se active si está desactivado
+    private void VerificarEstadoBT() {
+
+        if(btAdapter==null) {
+            Toast.makeText(getActivity(), "El dispositivo no soporta bluetooth", Toast.LENGTH_LONG).show();
+        } else {
+            if (btAdapter.isEnabled()) {
+            } else {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, 1);
+            }
+        }
+    }
+
+    //Crea la clase que permite crear el evento de conexion
+    private class ConnectedThread extends Thread
+    {
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket socket)
+        {
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+            try
+            {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) { }
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run()
+        {
+            byte[] byte_in = new byte[1];
+            // Se mantiene en modo escucha para determinar el ingreso de datos
+            while (true) {
+                try {
+                    mmInStream.read(byte_in);
+                    char ch = (char) byte_in[0];
+                    bluetoothIn.obtainMessage(handlerState, ch).sendToTarget();
+                } catch (IOException e) {
+                    break;
+                }
+            }
+        }
+
+        //Envio de trama
+        public void write(String input)
+        {
+            try {
+                mmOutStream.write(input.getBytes());
+            }
+            catch (IOException e)
+            {
+
+                Toast.makeText(getActivity(), "La Conexión fallo", Toast.LENGTH_LONG).show();
+
+                fragmentTransaction.replace(R.id.containerOfFragments, new StartCartFragment());
+                fragmentTransaction.commit();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/byd/aplication/ui/home/startEngine/devicesList/DevicesFragment.java
+++ app/src/main/java/com/example/byd/aplication/ui/home/startEngine/devicesList/DevicesFragment.java
@@ -1,25 +1,47 @@
 package com.example.byd.aplication.ui.home.startEngine.devicesList;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.byd.R;
+import com.example.byd.aplication.ui.MainActivity;
 import com.example.byd.aplication.ui.home.startEngine.control.ControlFragment;
 import com.example.byd.databinding.FragmentDevicesBinding;
+import com.google.android.material.snackbar.Snackbar;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+import java.util.Set;
+
+
 public class DevicesFragment extends Fragment {
+
+
+    private static final String TAG = "DevicesFragment";
+
+    ListView listViewPairedDevices;
+
+    public static String EXTRA_DEVICE_ADDRESS = "device_address";
+
+
+    private BluetoothAdapter mBtAdapter;
+    private ArrayAdapter mPairedDevicesArrayAdapter;
+
 
     public DevicesFragment() {
         // Required empty public constructor
@@ -36,9 +58,10 @@ public class DevicesFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         FragmentDevicesBinding fragmentDevicesBinding = FragmentDevicesBinding.inflate(inflater, container, false);
+        listViewPairedDevices = fragmentDevicesBinding.listViewDevices;
         fragmentManager = getParentFragmentManager();
         fragmentTransaction = fragmentManager.beginTransaction();
-
+//        View view = inflater.inflate(R.layout.fragment_devices, container, false);
         button = fragmentDevicesBinding.devicesButton;
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,4 +73,63 @@ public class DevicesFragment extends Fragment {
         return fragmentDevicesBinding.getRoot();
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        VerificarEstadoBT();
+        // Inicializa la array que contendra la lista de los dispositivos bluetooth vinculados
+        mPairedDevicesArrayAdapter = new ArrayAdapter(getActivity(), R.layout.custom_layout);
+
+        // Presenta los dispositivos vinculados en el ListView
+        listViewPairedDevices.setAdapter(mPairedDevicesArrayAdapter);
+        listViewPairedDevices.setOnItemClickListener(mDeviceClickListener);
+        // Obtiene el adaptador local Bluetooth adapter
+        mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+
+
+
+            Set<BluetoothDevice> pairedDevices = mBtAdapter.getBondedDevices();
+
+            // Adiciona un dispositivos emparejado al array
+            if (pairedDevices.size() > 0) {
+                for (BluetoothDevice device : pairedDevices) {
+                    mPairedDevicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+                }
+            }
+
+        //---------------------------------------------------------------------
+    }
+
+    // Configura un (on-click) para la lista
+    private AdapterView.OnItemClickListener mDeviceClickListener = new AdapterView.OnItemClickListener() {
+        public void onItemClick(AdapterView av, View v, int arg2, long arg3) {
+
+            // Obtener la dirección MAC del dispositivo, que son los últimos 17 caracteres en la vista
+            String info = ((TextView) v).getText().toString();
+            String address = info.substring(info.length() - 17);
+
+
+            // Realiza un intent para iniciar la siguiente actividad
+            // mientras toma un EXTRA_DEVICE_ADDRESS que es la dirección MAC.
+            fragmentTransaction.replace(R.id.containerOfFragments, new ControlFragment(address), "devices").addToBackStack("devices");
+            fragmentTransaction.commit();
+        }
+    };
+
+    private void VerificarEstadoBT() {
+        // Comprueba que el dispositivo tiene Bluetooth y que está encendido.
+        mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBtAdapter == null) {
+            Toast.makeText(getActivity(), "El dispositivo no soporta Bluetooth", Toast.LENGTH_SHORT).show();
+        } else {
+            if (mBtAdapter.isEnabled()) {
+                Log.d(TAG, "...Bluetooth Activado...");
+            } else {
+                //Solicita al usuario que active Bluetooth
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, 1);
+            }
+        }
+    }
 }
